shivchawla/unstoppable-wallet-android

Replace attributed colors with resource colors

app/src/main/java/io/horizontalsystems/bankwallet/ui/extensions/MarketMetricSmallView.kt
@@ -6,7 +6,6 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.App
-import io.horizontalsystems.views.helpers.LayoutHelper
 import kotlinx.android.synthetic.main.view_market_metric_small.view.*
 import java.math.BigDecimal
 
@@ -41,10 +40,8 @@ class MarketMetricSmallView @JvmOverloads constructor(context: Context, attrs: A
         val sign = if (v >= BigDecimal.ZERO) "+" else "-"
         diffPercentage.text = App.numberFormatter.format(v.abs(), 0, 2, sign, "%")
 
-        val textColor = if (v >= BigDecimal.ZERO) R.attr.ColorRemus else R.attr.ColorLucian
-        LayoutHelper.getAttr(textColor, context.theme)?.let {
-            diffPercentage.setTextColor(it)
-        }
+        val color = if (v >= BigDecimal.ZERO) R.color.remus else R.color.lucian
+        diffPercentage.setTextColor(context.getColor(color))
     }
 
     fun setMetricData(data: MetricData?) {

SAFE-anwang/SafeWallet-android

app/src/main/java/io/horizontalsystems/bankwallet/ui/extensions/MarketMetricSmallView.kt
@@ -6,7 +6,6 @@ import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.core.App
-import io.horizontalsystems.views.helpers.LayoutHelper
 import kotlinx.android.synthetic.main.view_market_metric_small.view.*
 import java.math.BigDecimal
 
@@ -41,7 +40,7 @@ class MarketMetricSmallView @JvmOverloads constructor(context: Context, attrs: A
         when {
             valueText != null -> {
                 value.text = valueText
-                LayoutHelper.getAttr(R.attr.ColorBran, context.theme)?.let { value.setTextColor(it) }
+                value.setTextColor(context.getColor(R.color.bran))
             }
             modeNotAvailable -> {
                 value.text = context.getString(R.string.NotAvailable)
@@ -60,13 +59,11 @@ class MarketMetricSmallView @JvmOverloads constructor(context: Context, attrs: A
 
                 val sign = if (diff >= BigDecimal.ZERO) "+" else "-"
                 diffPercentage.text = App.numberFormatter.format(diff.abs(), 0, 2, sign, "%")
-                val textColor = if (diff >= BigDecimal.ZERO) R.attr.ColorRemus else R.attr.ColorLucian
-                LayoutHelper.getAttr(textColor, context.theme)?.let {
-                    diffPercentage.setTextColor(it)
-                }
+                val textColor = if (diff >= BigDecimal.ZERO) R.color.remus else R.color.lucian
+                diffPercentage.setTextColor(context.getColor(textColor))
             }
             modeNotAvailable -> {
-                diffCircle.setBackground(LayoutHelper.getAttr(R.attr.ColorJeremy, context.theme, R.color.grey))
+                diffCircle.setBackground(context.getColor(R.color.jeremy))
                 diffPercentage.text = "----"
                 diffPercentage.setTextColor(context.getColor(R.color.grey_50))
             }