811560d3729cd7e83348d6d9944176daf4fa8e64
KernelBlock <38245617@qq.com>
2022-11-15 13:46:00 +0800

    Update SwapMainFragment.kt

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/SwapMainFragment.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/SwapMainFragment.kt
@@ -131,7 +131,11 @@ class SwapMainFragment : BaseFragment() {
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
-            mainViewModel.autoSetProvider1Inch()
+            childFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fragment_placeholder, SwapMainModule.OneInchProvider.fragment)
+                    .commitNow()
+           // mainViewModel.autoSetProvider1Inch()
         }
     }
 }
