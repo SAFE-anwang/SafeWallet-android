ba732461579659b41e0303ae310a5c2aaa8518e3
LongWen <416441008@qq.com>
2023-01-07 13:38:19 +0800

    修复数据库升级问题

--- app/src/main/java/io/horizontalsystems/bankwallet/core/storage/migrations/Migration_40_41.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/storage/migrations/Migration_40_41.kt
@@ -7,6 +7,20 @@ import io.horizontalsystems.bankwallet.entities.BtcRestoreMode
 
 object Migration_40_41 : Migration(40, 41) {
     override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("ALTER TABLE EnabledWallet ADD `coinName` TEXT")
+        database.execSQL("ALTER TABLE EnabledWallet ADD `coinCode` TEXT")
+        database.execSQL("ALTER TABLE EnabledWallet ADD `coinDecimals` INTEGER")
+
+        database.execSQL(
+            "UPDATE EnabledWallet " +
+                    "SET coinName = (SELECT coinName FROM CustomToken WHERE CustomToken.coinType = EnabledWallet.coinId), " +
+                    "coinCode = (SELECT coinCode FROM CustomToken WHERE CustomToken.coinType = EnabledWallet.coinId), " +
+                    "coinDecimals = (SELECT decimal FROM CustomToken WHERE CustomToken.coinType = EnabledWallet.coinId) " +
+                    "WHERE EXISTS (SELECT * FROM CustomToken WHERE CustomToken.coinType = EnabledWallet.coinId)"
+        )
+
+        database.execSQL("DELETE FROM CustomToken")
+
         val btcRestoreKey = BlockchainSettingsStorage.keyBtcRestore
         database.execSQL("CREATE TABLE IF NOT EXISTS `BlockchainSettingRecord` (`blockchainUid` TEXT NOT NULL, `key` TEXT NOT NULL, `value` TEXT NOT NULL, PRIMARY KEY(`blockchainUid`, `key`))")
 
@@ -57,5 +71,5 @@ private enum class BtcBlockchain(val raw: String) {
     BitcoinCash("bitcoinCash"),
     Litecoin("litecoin"),
     Dash("dash"),
-    Safe("SAFE");
+    Safe("safe-coin");
 }
