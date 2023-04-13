3f013cd70f6b4bacf5ef439765fb85a2ee5e3ab3
LongWen <416441008@qq.com>
2023-04-13 20:19:09 +0800

    Safe overview

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/ui/CoinScreenTitle.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/coin/ui/CoinScreenTitle.kt
@@ -1,12 +1,16 @@
 package io.horizontalsystems.bankwallet.modules.coin.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import io.horizontalsystems.bankwallet.R
 import io.horizontalsystems.bankwallet.ui.compose.ComposeAppTheme
 import io.horizontalsystems.bankwallet.ui.compose.components.CellSingleLineClear
 import io.horizontalsystems.bankwallet.ui.compose.components.CoinImage
@@ -21,11 +25,18 @@ fun CoinScreenTitle(
     iconPlaceholder: Int?
 ) {
     CellSingleLineClear {
-        CoinImage(
-            iconUrl = coinIconUrl,
-            placeholder = iconPlaceholder,
-            modifier = Modifier.size(24.dp)
-        )
+        if (coinIconUrl.endsWith("safe-coin@3x.png")) {
+            Image(painter = painterResource(id = R.drawable.logo_safe_24),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(24.dp))
+        } else {
+            CoinImage(
+                iconUrl = coinIconUrl,
+                placeholder = iconPlaceholder,
+                modifier = Modifier.size(24.dp)
+            )
+        }
 
         body_grey(
             text = coinName,
