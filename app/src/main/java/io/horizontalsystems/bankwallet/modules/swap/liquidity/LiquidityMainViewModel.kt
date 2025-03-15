fb233c5a97bf81fba14243756b3b74ccb84ad4f5
LongWen <416441008@qq.com>
2025-03-15 17:53:43 +0800

    Optimize and add liquidity performance

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/liquidity/LiquidityMainViewModel.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/swap/liquidity/LiquidityMainViewModel.kt
@@ -147,7 +147,7 @@ class LiquidityMainViewModel(
     private var refocusKey = UUID.randomUUID().leastSignificantBits
 
     var sendStateObservable = SingleLiveEvent<SendEvmTransactionService.SendState>()
-
+    var refreshState = false
 
     var swapState by mutableStateOf(
         LiquidityMainModule.SwapState(
@@ -308,6 +308,12 @@ class LiquidityMainViewModel(
             syncSwapDataState()
         }
         tradeServiceB.stateFlow.collectWith(viewModelScope) { state ->
+            if (state is SwapResultState.NotReady && refreshState) {
+                resyncSwapData()
+            }
+            if (state is SwapResultState.Ready) {
+                refreshState = false
+            }
             syncSwapDataState()
         }
 
@@ -350,6 +356,7 @@ class LiquidityMainViewModel(
                     is SwapData.UniswapData -> {
                         tradeView = uniswapTradeViewItem(swapData, fromTokenService.token, toTokenService.token)
                         if (exactType == ExactType.ExactFrom) {
+                            refreshState = true
                             amountTo = swapData.data.amountOut
                             toTokenService.onChangeAmount(swapData.data.amountOut.toString(), true)
                         } else {
@@ -489,10 +496,10 @@ class LiquidityMainViewModel(
     private fun syncButtonsState() {
         val revokeAction1 = getRevokeActionState()
         val approveAction1 = getApproveActionState(revokeAction1)
-        val proceedAction = getProceedActionState(revokeAction1)
 
         val revokeAction2 = getRevokeActionState2()
         val approveAction2 = getApproveActionState2(revokeAction2)
+        val proceedAction = getProceedActionState(revokeAction2)
 
         buttons = SwapMainModule.SwapButtons2(revokeAction1, revokeAction2, approveAction1, approveAction2, proceedAction)
         syncUiState()
