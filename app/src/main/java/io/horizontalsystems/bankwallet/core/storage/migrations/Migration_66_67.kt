e6000bed87936c7294d1ef84bdae8e943f5fe8a6
LongWen <416441008@qq.com>
2025-08-13 23:18:39 +0800

    fix database upgrade crash

--- app/src/main/java/io/horizontalsystems/bankwallet/core/storage/migrations/Migration_66_67.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/storage/migrations/Migration_66_67.kt
@@ -6,13 +6,18 @@ import io.horizontalsystems.bankwallet.core.App
 
 object Migration_66_67 : Migration(66, 67) {
     override fun migrate(database: SupportSQLiteDatabase) {
-
         database.execSQL("ALTER TABLE CustomToken RENAME TO TempCustomToken")
-        database.execSQL("CREATE TABLE IF NOT EXISTS `CustomToken` (`symbol` TEXT NOT NULL, " +
-                "`creator` TEXT NOT NULL, `type` INTEGER NOT NULL, `address` TEXT NOT NULL, " +
-                "`decimals` TEXT NOT NULL, `name` TEXT NOT NULL, `version` TEXT, " +
-                "`chainId` TEXT NOT NULL, `logoURI` TEXT NOT NULL, PRIMARY KEY(`address`))")
-        database.execSQL("INSERT INTO CustomToken (`symbol`, `creator`, `type`, `address`, `decimals`, `name`, `version`, `chainId`, `logoURI`) SELECT `symbol`, `creator`, `type`, `address`, `decimals`, `name`, `version`, `chainId`, `logoURI` FROM TempCustomToken")
+        database.execSQL(
+            "CREATE TABLE IF NOT EXISTS `CustomToken` (`symbol` TEXT NOT NULL, " +
+                    "`creator` TEXT NOT NULL, `type` INTEGER NOT NULL, `address` TEXT NOT NULL, " +
+                    "`decimals` TEXT NOT NULL, `name` TEXT NOT NULL, `version` TEXT, " +
+                    "`chainId` TEXT NOT NULL, `logoURI` TEXT NOT NULL, PRIMARY KEY(`address`))"
+        )
+        try {
+            database.execSQL("INSERT INTO CustomToken (`symbol`, `creator`, `type`, `address`, `decimals`, `name`, `version`, `chainId`, `logoURI`) SELECT `symbol`, `creator`, `type`, `address`, `decimals`, `name`, `version`, `chainId`, `logoURI` FROM TempCustomToken")
+        } catch (e: Exception) {
+
+        }
         database.execSQL("DROP TABLE TempCustomToken")
     }
 }
