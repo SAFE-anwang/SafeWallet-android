17b27d6c347e3a022aed3fa4aee5c4b287bb2506
LongWen <416441008@qq.com>
2025-09-20 13:56:26 +0800

    fix database upgrade

--- app/src/main/java/io/horizontalsystems/bankwallet/core/storage/migrations/Migration_67_68.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/core/storage/migrations/Migration_67_68.kt
@@ -14,7 +14,7 @@ object Migration_67_68 : Migration(67, 68) {
         )
         database.execSQL(
             "CREATE TABLE IF NOT EXISTS `ProposalRecordInfo` (`id` INTEGER NOT NULL, " +
-                    "`creator` TEXT NOT NULL, `title` TEXT, `payAmount` TEXT NOT NULL, " +
+                    "`creator` TEXT NOT NULL, `title` TEXT NOT NULL, `payAmount` TEXT NOT NULL, " +
                     "`payTimes` INTEGER NOT NULL, `startPayTime` INTEGER NOT NULL, `endPayTime` INTEGER NOT NULL, " +
                     "`description` TEXT NOT NULL, `state` INTEGER NOT NULL, `createHeight` INTEGER, `updateHeight` INTEGER,  `newProposal` INTEGER NOT NULL,  PRIMARY KEY(`id`))"
         )
