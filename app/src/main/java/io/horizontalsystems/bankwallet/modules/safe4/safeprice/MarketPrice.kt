7047b5b683739e2a16e62e3658a3ded0406d35a7
Long <416441008@qq.com>
2026-01-15 23:11:10 +0800

    src20 show price

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/safe4/safeprice/MarketPrice.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/safe4/safeprice/MarketPrice.kt
@@ -5,7 +5,7 @@ data class MarketPrice(
     val decimals: Int,
     val name: String,
     val symbol: String,
-    val price: String?,
+    val price: String,
     val change: String,
     val logoURI: String,
     val usdtReserves: String,
