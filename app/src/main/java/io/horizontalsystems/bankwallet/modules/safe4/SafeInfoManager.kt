7487464a6591a6bcb2d7e47109d9fe4df9ff126a
Long <416441008@qq.com>
2026-02-04 21:11:14 +0800

    fix send src20 lock tx error

--- app/src/main/java/io/horizontalsystems/bankwallet/modules/safe4/SafeInfoManager.kt
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/safe4/SafeInfoManager.kt
@@ -68,7 +68,6 @@ object SafeInfoManager {
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                Log.d("longwen", "usdt=$it")
                 saveUsdtMmkv(it)
             }, {
                 Log.e("safe4", "getSafeInfo error", it)
